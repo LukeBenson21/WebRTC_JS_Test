544548166a01dff46ca36d95ca33bcd5ad9f4c96
Luke Benson <ldunkinb@gmail.com>
2022-03-24 16:42:48 +0000

    Update helper_script.js

--- helper_script.js
+++ helper_script.js
@@ -93,7 +93,7 @@ function gotRemoteStream(event, peerUuid) {
   //var vidElement = document.createElement('video');
   //vidElement.setAttribute('autoplay', '');
   //vidElement.setAttribute('muted', '');
-  vidElement.srcObject = event.streams[0];
+  //vidElement.srcObject = event.streams[0];
 
   //var vidContainer = document.createElement('div');
   //vidContainer.setAttribute('id', 'remoteVideo_' + peerUuid);
